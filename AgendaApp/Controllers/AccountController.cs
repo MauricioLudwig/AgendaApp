1724858732a40a3e3fe681a545aa9dc7fc20929e
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2018-08-03 08:23:58 +0200

    Basic account views added

--- AgendaApp/Controllers/AccountController.cs
+++ AgendaApp/Controllers/AccountController.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AgendaApp.Data;
+using AgendaApp.Data.Entities;
 using AgendaApp.Models;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AgendaApp.Controllers
@@ -15,41 +19,137 @@ namespace AgendaApp.Controllers
     {
 
         private AgendaDbContext context;
+        private UserManager<ApplicationUser> userManager;
+        private SignInManager<ApplicationUser> signInManager;
+        private IMapper mapper;
+        private string userId { get; set; }
 
-        public AccountController(AgendaDbContext context)
+        public AccountController(AgendaDbContext context, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IMapper mapper, IHttpContextAccessor httpContextAccessor)
         {
             this.context = context;
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.mapper = mapper;
+            var user = httpContextAccessor.HttpContext.User;
+            userId = userManager.GetUserId(user);
         }
 
         [AllowAnonymous]
         [HttpGet]
-        public IActionResult Login()
+        public IActionResult Login(string returnUrl = "")
         {
-            return Ok();
+            var alreadySignedIn = signInManager.IsSignedIn(User);
+
+            if (alreadySignedIn)
+                return RedirectToAction("", "");
+
+            var model = new LoginVM
+            {
+                ReturnUrl = returnUrl
+            };
+
+            return View(model);
         }
 
         [AllowAnonymous]
-        [HttpGet]
-        public IActionResult Login(int id)
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM model)
         {
-            return Ok();
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var userNameExists = await userManager.FindByNameAsync(model.UserName);
+            var emailExists = await userManager.FindByEmailAsync(model.UserName);
+
+            // check username and password sign-in
+            if (userNameExists != null)
+            {
+                var result = await signInManager.PasswordSignInAsync(userNameExists, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                        return Redirect(model.ReturnUrl);
+                    else
+                        return RedirectToAction("", "");
+                }
+            }
+
+            // Check email and password sign-in
+            if (emailExists != null)
+            {
+                var result = await signInManager.PasswordSignInAsync(emailExists, model.Password, model.RememberMe, false);
+                
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                        return Redirect(model.ReturnUrl);
+                    else
+                        return RedirectToAction("", "");
+                }
+            }
+
+            ModelState.AddModelError("", "Incorrect username or password");
+            return View(model);
         }
 
         [AllowAnonymous]
         [HttpGet]
         public IActionResult Register()
         {
+            var alreadySignedIn = signInManager.IsSignedIn(User);
+            if (alreadySignedIn)
+                return RedirectToAction("", "");
+
             return View();
         }
 
         [AllowAnonymous]
         [HttpPost]
-        public IActionResult Register(RegisterVM model)
+        public async Task<IActionResult> Register(RegisterVM model)
         {
             if (!ModelState.IsValid)
                 return View(model);
 
-            return Ok();
+            var userNameExists = await userManager.FindByNameAsync(model.UserName);
+            if (userNameExists != null)
+                ModelState.AddModelError("", "The username you have entered already exists.");
+
+            var emailExists = await userManager.FindByEmailAsync(model.Email);
+            if (emailExists != null)
+                ModelState.AddModelError("", "The email you have entered already exists.");
+
+            if (userNameExists != null || emailExists != null)
+                return View();
+
+            var user = new ApplicationUser
+            {
+                UserName = model.UserName,
+                Email = model.Email
+            };
+            var result = await userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                await signInManager.SignInAsync(user, false);
+                return RedirectToAction("", "");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("", "");
         }
 
     }
---
+++ AgendaApp/Models/Account/LoginVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgendaApp.Models
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage = "Username is required")]
+        [Display(Name = "Username or email")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
+    }
+}
