51f15dbb28834fbaddd0a018c2036edd899bb26c
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2018-08-08 12:33:02 +0200

    home, register layout

--- AgendaApp/Controllers/AccountController.cs
+++ AgendaApp/Controllers/AccountController.cs
@@ -34,58 +34,6 @@ namespace AgendaApp.Controllers
             userId = userManager.GetUserId(user);
         }
 
-        [AllowAnonymous]
-        [HttpGet]
-        public IActionResult Register()
-        {
-            var alreadySignedIn = signInManager.IsSignedIn(User);
-            if (alreadySignedIn)
-                return RedirectToAction("Index", "Profile");
-
-            return View();
-        }
-
-        [AllowAnonymous]
-        [HttpPost]
-        public async Task<IActionResult> Register(RegisterVM model)
-        {
-            if (!ModelState.IsValid)
-                return View(model);
-
-            var userNameExists = await userManager.FindByNameAsync(model.UserName);
-            if (userNameExists != null)
-                ModelState.AddModelError("", "The username you have entered already exists.");
-
-            var emailExists = await userManager.FindByEmailAsync(model.Email);
-            if (emailExists != null)
-                ModelState.AddModelError("", "The email you have entered already exists.");
-
-            if (userNameExists != null || emailExists != null)
-                return View();
-
-            var user = new ApplicationUser
-            {
-                UserName = model.UserName,
-                Email = model.Email
-            };
-            var result = await userManager.CreateAsync(user, model.Password);
-
-            if (result.Succeeded)
-            {
-                await signInManager.SignInAsync(user, false);
-                return RedirectToAction("Index", "Profile");
-            }
-            else
-            {
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError("", error.Description);
-                }
-            }
-
-            return View();
-        }
-
         [HttpGet]
         public async Task<IActionResult> Logout()
         {
--- AgendaApp/Controllers/HomeController.cs
+++ AgendaApp/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace AgendaApp.Controllers
             this.signInManager = signInManager;
         }
 
+        #region Sign in
         [HttpGet]
         public IActionResult Index()
         {
@@ -57,6 +58,60 @@ namespace AgendaApp.Controllers
             ModelState.AddModelError("", "Incorrect username or password");
             return View(model);
         }
+        #endregion
+
+        #region Sign up
+        [HttpGet]
+        public IActionResult Register()
+        {
+            var alreadySignedIn = signInManager.IsSignedIn(User);
+
+            if (alreadySignedIn)
+                return RedirectToAction("Index", "Profile");
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var userNameExists = await userManager.FindByNameAsync(model.UserName);
+            if (userNameExists != null)
+                ModelState.AddModelError("", "The username you have entered already exists.");
+
+            var emailExists = await userManager.FindByEmailAsync(model.Email);
+            if (emailExists != null)
+                ModelState.AddModelError("", "The email you have entered already exists.");
+
+            if (userNameExists != null || emailExists != null)
+                return View();
+
+            var user = new ApplicationUser
+            {
+                UserName = model.UserName,
+                Email = model.Email
+            };
+            var result = await userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                await signInManager.SignInAsync(user, false);
+                return RedirectToAction("Index", "Profile");
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View();
+        }
+        #endregion
 
     }
 }
